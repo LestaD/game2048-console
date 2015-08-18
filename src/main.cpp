e8ee3cc01fe5563884507f78a5f02be733800b0d
LestaD <i.am@Lestad.net>
2015-08-18 19:17:22 +0300

    Makefile

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game2048-console
+//
+//  Created by LestaD on 18.08.15.
+//  Copyright (c) 2015 LestaD. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
